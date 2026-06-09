253ef548a0608fe22b8cb599f39d66cb293b13dc
Jakaria Molla <jakariaee@gmail.com>
2026-06-09 21:22:12 +0600

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,4 +1,3 @@
-# FILE: core/views.py
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
